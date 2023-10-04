c100f3b0e2b5480b4e59e775b5119750b8e83635
Jay <85805386+junaid-cs@users.noreply.github.com>
2023-10-04 22:32:54 +0500

    add testimonal

--- automobiz/src/components/Testimoinal/Testimonal.jsx
+++ automobiz/src/components/Testimoinal/Testimonal.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import "./Testimonal.css"
 
-const Testimonal = () => {
+function Testimonal() {
+
   return (
-    <div>Testimonal</div>
-  )
+    <div className="text-white">
+     <Carousel>
+                <div>
+                    <h3>carousel 1</h3>
+                    <p className="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, inventore!</p>
+                </div>
+                <div>
+                    <p className="my-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, inventore!</p>
+                <h3>carousel 1</h3>
+                </div>
+                <div>
+                <h3>carousel 1</h3>
+                    <p className="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, inventore!</p>
+                </div>
+            </Carousel>
+    </div>
+  );
 }
 
-export default Testimonal
+export default Testimonal;
